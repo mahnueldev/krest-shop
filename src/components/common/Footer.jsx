e55430f09aae36c03e7782ad09a2d18c771274ed
mahnueldev <emmanuelgbd3@gmail.com>
2023-02-27 07:31:22 +0100

    first commit

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>Copyright © KREST all rights reserved. Powered by mahnuel.</p>
+      </footer>
+    </>
+  )
+}
